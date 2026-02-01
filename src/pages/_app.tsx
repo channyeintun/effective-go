0c7d0f37645ac4119d831bbddb1a5ad31e957c82
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2026-02-01 08:38:55 +0700

    fix: update font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Noto_Sans_Myanmar } from "next/font/google";
+
+const notoMyanmar = Noto_Sans_Myanmar({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["myanmar"],
+  variable: "--font-noto-myanmar",
+  display: 'block',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={notoMyanmar.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
